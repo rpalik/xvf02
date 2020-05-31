5e2d196929ee941dcc3b9673649e24c982207a32
rpalik <rpalik@gmail.com>
2020-05-31 23:20:59 +0200

    .

---
+++ xvf02/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xvf02
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string text = MainEntry.Text;
+            MainLabel.Text = "Hello " + text;
+
+        }
+
+        private async void NextPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+
+        }
+    }
+}
